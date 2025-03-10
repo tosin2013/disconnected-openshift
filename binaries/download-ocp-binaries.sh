b997ecb1002562dc48d9d5368296b2a63a69c520
Ken Moini <ken@kenmoini.com>
2025-03-10 06:44:16 -0400

    who knows

--- binaries/download-ocp-binaries.sh
+++ binaries/download-ocp-binaries.sh
@@ -1,27 +1,91 @@
 #!/bin/bash
 
 # This script downloads the latest OCP binaries for the current platform
-# and extracts them into the ./bin directory.
+# and extracts them into the system or local ./bin directory.
 
-ARCH="x86_64"
-OS="linux"
+DEST_MODE=${DEST_MODE:="local"} # local or system
+CHANNEL=${CHANNEL:="latest"} # latest, stable, stable-4.20, etc
+BUTANE_CHANNEL=${BUTANE_CHANNEL:="latest"} # latest, v0.23.0-0, etc
 
-mkdir -p ./bin/tmp
-cd ./bin/tmp
+# Set the target platform if not set
+if [ -z "$TARGETPLATFORM" ]; then
+  ARCH=$(arch)
+  OS=$(uname -o | sed 's|GNU/L|l|')
+  TARGETPLATFORM="$OS/$ARCH"
+fi
 
-wget https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/stable/openshift-client-${OS}.tar.gz
-wget https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/stable/openshift-install-${OS}.tar.gz
+# Set the filenames based on the platform - it's not really a standard...
+if [ "$TARGETPLATFORM" = "linux/amd64" ] || [ "$TARGETPLATFORM" = "linux/x86_64" ]; then
+  ARCH=x86_64
+  IARCH=amd64
+  OPENSHIFT_INSTALL_FILENAME=openshift-install-linux
+  OPENSHIFT_CLIENT_FILENAME=openshift-client-linux
+  BUTANE_FILENAME=butane-${IARCH}
+elif [ "$TARGETPLATFORM" = "linux/arm64" ] || [ "$TARGETPLATFORM" = "linux/aarch64" ]; then
+  ARCH=arm64
+  IARCH=aarch64
+  OPENSHIFT_INSTALL_FILENAME=openshift-install-linux-arm64
+  OPENSHIFT_CLIENT_FILENAME=openshift-client-linux-arm64
+  BUTANE_FILENAME=butane-${IARCH}
+elif [ "$TARGETPLATFORM" = "Darwin/arm64" ] || [ "$TARGETPLATFORM" = "Darwin/aarch64" ]; then
+  ARCH=arm64
+  IARCH=aarch64
+  OPENSHIFT_INSTALL_FILENAME=openshift-install-mac-arm64
+  OPENSHIFT_CLIENT_FILENAME=openshift-client-mac-arm64
+  # Butane doesn't have a darwin build for arm64?
+  BUTANE_FILENAME=butane-darwin-amd64
+else
+  echo "$TARGETPLATFORM - Building for unsupported platform"
+  exit 1
+fi
 
-tar zxvf openshift-client-${OS}.tar.gz
-tar zxvf openshift-install-${OS}.tar.gz
+# Set the destination directory
+if [ "$DEST_MODE" = "system" ]; then
+  DEST_DIR="/usr/local/bin"
+else
+  DEST_DIR="./bin"
+fi
 
-chmod a+x oc
-chmod a+x kubectl
-chmod a+x openshift-install
+# Print some debug info
+echo "$TARGETPLATFORM - Building for ARCH: $ARCH | IARCH: $IARCH | OS: $OS"
+echo "Operating in $DEST_MODE mode, extracting to $DEST_DIR"
 
-mv oc ../
-mv kubectl ../
-mv openshift-install ../
+# Create a temporary directory to download and extract the binaries
+mkdir -p $DEST_DIR/ocpbintmp
+cd $DEST_DIR/ocpbintmp
 
+# Get Base and common multi-arch files
+# Butane defaults to latest since it doesn't follow OCP versions
+wget https://mirror.openshift.com/pub/openshift-v4/clients/butane/${BUTANE_CHANNEL}/${BUTANE_FILENAME}
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${CHANNEL}/${OPENSHIFT_INSTALL_FILENAME}.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${CHANNEL}/${OPENSHIFT_CLIENT_FILENAME}.tar.gz
+
+# Get additional files for x86_64 - rather things that don't have an Arm64 build
+if [ "$ARCH" = "x86_64" ] && [ "$OS" = "linux" ]; then
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${CHANNEL}/oc-mirror.rhel9.tar.gz
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${CHANNEL}/opm-linux-rhel9.tar.gz
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${CHANNEL}/ccoctl-linux.tar.gz
+fi
+
+# Extract the files
+for t in *.tar.gz; do
+  tar zxvf $t
+  rm -f $t
+  rm -f README.md
+done
+
+# Normalize names and set some permissions
+mv ${BUTANE_FILENAME} butane
+chmod a+x oc kubectl openshift-install butane
+mv oc kubectl openshift-install butane ..
+
+# Additional files for x86_64
+if [ "$ARCH" = "x86_64" ]; then
+  mv opm-rhel9 opm
+  chmod a+x oc-mirror opm ccoctl
+  mv oc-mirror opm ccoctl ..
+fi
+
+# Clean up
 cd ..
-rm -rf tmp
+rm -rf ocpbintmp
